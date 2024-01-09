453335d50245ad5d2265329f8bcf47e44d498f65
chewlabSB2 <87451986+chewlabSB2@users.noreply.github.com>
2024-01-09 15:46:38 +0800

    Update RFdiffusion_bash.sh

--- helper_scripts/template_scripts/RFdiffusion_bash.sh
+++ helper_scripts/template_scripts/RFdiffusion_bash.sh
@@ -8,11 +8,15 @@
 #SBATCH --error=%x-%j.err
 #SBATCH --partition=gpu4w
 
+module load cuda/12.1 #load CUDA
+
 # Activate the Conda environment for RFdiffusion
 source /home/users/astar/gis/your_username/scratch/miniforge3/etc/profile.d/conda.sh
 conda activate SE3nv
-# Print the path to the Python executable of the activated environment
-python -c "import sys; print(sys.executable)"
+
+# Set PYTHONPATH to include the RFdiffusion directory
+export PYTHONPATH="${PYTHONPATH}:/home/users/astar/gis/your_username/scratch/denovo_editors/RFdiffusion"
+
 # Navigate to the RFdiffusion directory (adjust path as needed)
-cd /home/users/astar/gis/your_username/scratch/denovo_editors/RFdiffusion/
+cd /home/users/astar/gis/diyasri/scratch/denovo_editors/RFdiffusion/
 /home/users/astar/gis/your_username/scratch/miniforge3/envs/SE3nv/bin/python ./scripts/run_inference.py 'contigmap.contigs=[add]' inference.input_pdb=inputs/your.pdb contigmap.length=add inference.output_prefix=outputs/run_number/ diffuser.T=100 inference.num_designs=100 inference.ckpt_override_path=models/Complex_base_ckpt.pt
